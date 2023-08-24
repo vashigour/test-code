2c358fa8f448f62d35d1f28d59046a05b7e540e8
vashigour <vashibrdc20@gmail.com>
2023-08-24 11:12:16 +0530

    code-update

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-// // // let ary = [20,25,30,53];
-// // // for(var a=0; a<4 ;a++){
-// // // document.write(ary[a] +"<br>");
-// // // }
-// // // function clicked(){
-// // //    var a =5;
-// // //    var b =10;
-// // //    document.write(a+b);
-// // // }
-// // // function hover(){
-// // //     document.write('you are hover here')
-// // //  }
-// // container.addEventListener('click',function(){
-// //      document.write("clicked");
-// // })
-// // function date(){
-// //     date = new Date();
-// //     document.write(date);
-// // }
-//  let age = 24;
-// if (age>18){
-//     document.write('you can drive')
-// }else{
-//     document.write('you cannot drive')
-// }
-
-// console.log(2+'2);
-function sum(a,b){
-     return a+b;
+let ary = [20,25,30,53];
+for(var a=0; a<4 ;a++){
+document.write(ary[a] +"<br>");
 }
-sum(3,5);
+function clicked(){
+   var a =5;
+   var b =10;
+   document.write(a+b);
+}
+function hover(){
+    document.write('you are hover here')
+ }
+container.addEventListener('click',function(){
+     document.write("clicked");
+})
+function date(){
+    date = new Date();
+    document.write(date);
+}
+ let age = 24;
+if (age>18){
+    document.write('you can drive')
+}else{
+    document.write('you cannot drive')
+}
